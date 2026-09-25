1e74a1ba3ecebb93fa78f891f6f5dbd9373ba195
Joao Matias <jdmatias@live.com>
2026-09-25 07:58:53 +0100

    Host section added

--- client/src/components/host/host.component.jsx
+++ client/src/components/host/host.component.jsx
@@ -1,17 +1,29 @@
 import style from './host.module.css';
 
 const Host = ({ host: { user } }) => {
-  const { avatar, first_name: firstName, surname } = user;
+  const { avatar, first_name: firstName, email, phone_number: phoneNumber, created_at: createdAt } = user;
+
+  const difInMilSecs = new Date() - new Date(createdAt);
+  const msInYear = 1000 * 60 * 60 * 24 * 365.25;
+  const yearsMember = Math.floor(difInMilSecs / msInYear);
+
+  console.log(user);
 
   return (
     <div className={style.propertyDetails}>
       <div>
         <h2 className={style.propertySubTitle}>Host</h2>
-        <section className={style.hostContainer}>
-          <img src={avatar} className={style.img} />
-          <p className={style.hostName}>
-            {firstName} {surname}
-          </p>
+        <section className={style.hostDetails}>
+          <div className={style.hostImgContainer}>
+            <img src={avatar} className={style.img} />
+            <p className={style.hostName}>{firstName}</p>
+          </div>
+
+          <div className={style.hostInfoContainer}>
+            <p className={style.boldDetails}>{yearsMember}</p>
+            <p className={style.hostPText}>year{yearsMember > 1 ? 's' : ''} as a member</p>
+            <p></p>
+          </div>
         </section>
       </div>
     </div>
